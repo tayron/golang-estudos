06432467ee91009c8ff2e355c65ac80c6e643c5a
Tayron Miranda <contato@tayron.com.br>
2020-06-26 14:08:11 -0300

    Adicionando exemplo dos tipos zeros

---
+++ zeros/zeros.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	var b float64
+	var c bool
+	var d string
+	var e *int
+
+	fmt.Printf("%v %v %v %v %v", a, b, c, d, e)
+	fmt.Printf("%v %v %v %q %v", a, b, c, d, e)
+}
