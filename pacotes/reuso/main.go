3cb2a88cb7b351ab106e29acdea085700554e98d
Tayron Miranda <contato@tayron.com.br>
2020-07-06 08:20:24 -0300

    Adicionando exemplo de reutilização de pacotes

---
+++ pacotes/reuso/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/tayron/area"
+)
+
+func main() {
+	fmt.Println(area.Circ(6.0))
+	fmt.Println(area.Rect(5.2, 2.0))
+	//fmt.Println(area._TrianguloEq(5.0, 2.0))
+}
