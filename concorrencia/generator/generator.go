19474562a2979647a3e2d3e4ca57386cea3a2b25
Tayron Miranda <contato@tayron.com.br>
2020-07-08 20:10:36 -0300

    Correção nas função que recupera titulo das páginas html

--- concorrencia/generator/generator.go
+++ concorrencia/generator/generator.go
@@ -19,7 +19,14 @@ func titulo(urls ...string) <-chan string {
 			html, _ := ioutil.ReadAll(resp.Body)
 
 			r, _ := regexp.Compile("<title>(.*?)<\\/title>")
-			c <- r.FindStringSubmatch(string(html))[1]
+			title := r.FindStringSubmatch(string(html))
+
+			if len(title) > 0 {
+				c <- r.FindStringSubmatch(string(html))[1]
+			} else {
+				mensagem := "Não foi possível encontrar titulo para a url: " + url
+				c <- mensagem
+			}
 		}(url)
 
 	}
