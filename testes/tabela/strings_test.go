10c0dc271ee5a2ebb07d575d439b6d399e15ae08
Tayron Miranda <contato@tayron.com.br>
2020-07-08 21:27:54 -0300

    Adicionado exemplo de criação de dataset para testes unitários

---
+++ testes/tabela/strings_test.go
@@ -0,0 +1,32 @@
+package strings
+
+import (
+	"strings"
+	"testing"
+)
+
+const msgIndex = "%s (part: %s) - índices: esperado (%d) <> encontrado (%d)."
+
+func TestIndex(t *testing.T) {
+	// Criando um dataset para testar vários cenários
+	// go test -v / Executando testes no modo verboso
+	testes := []struct {
+		texto    string
+		parte    string
+		esperado int
+	}{
+		{"Cod3r é show", "Cod3r", 0},
+		{"", "", 0},
+		{"Opa", "opa", -1},
+		{"leonardo", "o", 2},
+	}
+
+	for _, teste := range testes {
+		t.Logf("Massa: %v", teste)
+		atual := strings.Index(teste.texto, teste.parte)
+
+		if atual != teste.esperado {
+			t.Errorf(msgIndex, teste.texto, teste.parte, teste.esperado, atual)
+		}
+	}
+}
