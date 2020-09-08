d3ae7167c157ec88e486bf6bdde5b872976a07df
Tayron Miranda <contato@tayron.com.br>
2020-09-08 17:19:44 -0300

    Adicionado exemplo de expanser com internacionalização de dinheiro

---
+++ expander/go.mod
@@ -0,0 +1,5 @@
+module github.com/tayron/go-lang-estudos/expander
+
+go 1.14
+
+require github.com/leekchan/accounting v1.0.0
---
+++ expander/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"math/big"
+	"net/http"
+
+	"github.com/tayron/go-lang-estudos/expander/expanders"
+)
+
+// Produto -
+type Produto struct {
+	Nome  string
+	Preco *big.Float
+}
+
+func createTemplate(fileName string) *template.Template {
+	pathToFile := fmt.Sprintf("./pages/%s.html", fileName)
+	templateBase := template.New("base.html").Funcs(expanders.All())
+	tplHome, error := templateBase.ParseFiles("./pages/base.html", pathToFile)
+	return template.Must(tplHome, error)
+}
+
+// HomeHandler -
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	parameters := struct {
+		PageTitle string
+		Produtos  []Produto
+	}{
+		PageTitle: "Produtos",
+		Produtos: []Produto{
+			{Nome: "Arroz", Preco: big.NewFloat(15.23)},
+			{Nome: "Feijão", Preco: big.NewFloat(5.21)},
+		},
+	}
+
+	template := createTemplate("home")
+	template.Execute(w, parameters)
+}
+
+func main() {
+
+	http.HandleFunc("/", HomeHandler)
+	fmt.Println("Servidor startado no endereço: http://127.0.0.1:8182")
+	panic(http.ListenAndServe("127.0.0.1:8182", nil))
+
+}
