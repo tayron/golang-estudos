9afcabac3b7084b660511a08c70e2cf43c0a3603
Tayron Miranda <contato@tayron.com.br>
2020-09-19 15:54:15 -0300

    Adicionado exemplo de como pegar ip da maquina

---
+++ get_local_ip/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	fmt.Printf("IP da minha máquina na rede é: %s \n", GetLocalIP())
+}
+
+func GetLocalIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback the display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
+}
