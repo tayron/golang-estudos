a12b82062a908c9ff86cd6bb510fc7529302ec79
Tayron Miranda <contato@tayron.com.br>
2020-09-25 09:31:19 -0300

    Adicionado exemplo com webview em full screen

--- desktop/lorca/main.go
+++ desktop/lorca/main.go
@@ -8,10 +8,13 @@ import (
 
 func main() {
 	// Create UI with basic HTML passed via data URI
-	ui, err := lorca.New("data:text/html", "", 480, 320)
+	ui, err := lorca.New("http://www.google.com.br", "", 0, 0)
 	if err != nil {
 		log.Fatal(err)
 	}
+	ui.SetBounds(lorca.Bounds{
+		WindowState: "fullscreen",
+	})
 	defer ui.Close()
 	// Wait until UI window is closed
 	<-ui.Done()
