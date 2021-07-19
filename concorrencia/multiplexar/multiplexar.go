8cdb643acd11760f83dbf4f0f21643a3402e9f7b
Tayron Miranda <contato@tayron.com.br>
2021-07-19 15:20:29 -0300

    Ajuste exemplo multiplexar

--- concorrencia/multiplexar/multiplexar.go
+++ concorrencia/multiplexar/multiplexar.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/tayron/go-lang-estudos/html"
+	"io/ioutil"
+	"net/http"
+	"regexp"
 )
 
 func encaminhar(origem <-chan string, destino chan string) {
@@ -24,10 +25,34 @@ func juntar(entrada1, entrada2 <-chan string) <-chan string {
 
 func main() {
 	c := juntar(
-		html.Titulo("https://www.cod3r.com.br", "https://www.google.com"),
-		html.Titulo("https://www.amazon.com", "https://www.youtube.com"),
+		titulo("https://www.cod3r.com.br", "https://www.google.com"),
+		titulo("https://www.amazon.com", "https://www.youtube.com"),
 	)
 
 	fmt.Println("Primeiros:", <-c, "|", <-c)
 	fmt.Println("Segundos:", <-c, "|", <-c)
+}
+
+func titulo(urls ...string) <-chan string {
+	c := make(chan string)
+
+	for _, url := range urls {
+		go func(url string) {
+			resp, _ := http.Get(url)
+			html, _ := ioutil.ReadAll(resp.Body)
+
+			r, _ := regexp.Compile("<title>(.*?)<\\/title>")
+			title := r.FindStringSubmatch(string(html))
+
+			if len(title) > 0 {
+				c <- r.FindStringSubmatch(string(html))[1]
+			} else {
+				mensagem := "Não foi possível encontrar titulo para a url: " + url
+				c <- mensagem
+			}
+		}(url)
+
+	}
+
+	return c
 }
