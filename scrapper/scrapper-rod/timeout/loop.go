b35723e0f7a856f28d613471d4ba5b2353a3b6bd
Tayron Miranda <tayron.miranda.ext@beneficiofacil.com.br>
2023-06-21 15:03:24 -0300

    Exemplo de tratamento de timeout dentro de um loop

---
+++ scrapper/scrapper-rod/timeout/loop.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/go-rod/rod"
+)
+
+func main() {
+	const TIMEOUT = 0
+	const PAGINA = "https://www.hospeda.app"
+
+	timeout := 0
+	i := 0
+	for {
+
+		i++
+		err := rod.Try(func() {
+			getPageHTML(PAGINA, timeout)
+		})
+
+		if errors.Is(err, context.DeadlineExceeded) {
+			fmt.Printf("Página %s demorou mais que %d segundos para abrir \n", PAGINA, timeout)
+			timeout = 1
+			continue
+		} else if err != nil {
+			fmt.Printf("Erro desconhecido: %s \n", err.Error())
+			timeout = 1
+			continue
+		}
+
+		fmt.Printf("OK: %d \n", i)
+		timeout = 0
+	}
+
+}
+
+func getPageHTML(pagina string, timeout int) string {
+	timeoutSeconds := time.Second * time.Duration(timeout)
+	page := rod.New().MustConnect().MustPage()
+	page.Timeout(timeoutSeconds).MustNavigate(pagina)
+	return page.MustHTML()
+}
