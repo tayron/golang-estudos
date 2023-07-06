6743733914e63eaed4ded8a6cea1e1491a7bf7f3
Tayron Miranda <tayron.miranda.ext@beneficiofacil.com.br>
2023-07-06 16:39:41 -0300

    Adicionado exemplo com colly

---
+++ scrapper/scrapper-colly/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+
+	"github.com/gocolly/colly"
+)
+
+type Fact struct {
+	ID          int    `json:"id"`
+	Description string `json:"description"`
+}
+
+func main() {
+	allFacts := make([]Fact, 0)
+
+	collector := colly.NewCollector(
+		colly.AllowedDomains("factretriever.com", "www.factretriever.com"),
+	)
+
+	collector.OnHTML(".factsList li", func(element *colly.HTMLElement) {
+		factId, err := strconv.Atoi(element.Attr("id"))
+		if err != nil {
+			log.Println("Could not get id")
+		}
+
+		factDesc := element.Text
+
+		fact := Fact{
+			ID:          factId,
+			Description: factDesc,
+		}
+
+		allFacts = append(allFacts, fact)
+	})
+
+	collector.OnRequest(func(request *colly.Request) {
+		fmt.Println("Visiting", request.URL.String())
+	})
+
+	collector.Visit("https://www.factretriever.com/rhino-facts")
+
+	writeJSON(allFacts)
+}
+
+func writeJSON(data []Fact) {
+	file, err := json.MarshalIndent(data, "", " ")
+	if err != nil {
+		log.Println("Unable to create json file")
+		return
+	}
+
+	_ = ioutil.WriteFile("rhinofacts.json", file, 0644)
+}
